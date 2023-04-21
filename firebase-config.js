c893cd75aa2390fdcd797ba482dcfb87b6dfe132
Kathryn Jerez <kathrynjerez@gmail.com>
2023-04-21 11:04:14 -0400

    environment setup

--- firebase-config.js
+++ firebase-config.js
@@ -9,9 +9,9 @@ import { getFirestore } from '@firebase/firestore';
 const firebaseConfig = {
   apiKey: '${process.env.REACT_APP_FIREBASE_API_KEY}',
   authDomain: '${process.env.REACT_APP_FIREBASE_AUTH_DOMAIN}',
-  projectId: 'process.env.REACT_APP_FIREBASE_PROJECT_ID',
+  projectId: 'lighthall-todo',
   storageBucket: '${process.env.REACT_APP_FIREBASE_STORAGE_BUCKET}',
-  messagingSenderId: "${process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID}",
+  messagingSenderId: '${process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID}',
   appId: '${process.env.REACT_APP_FIREBASE_APP_ID}',
   measurementId: '${process.env.REACT_APP_FIREBASE_MEASUREMENT_ID}'
 };
