a9e335b6df360c643ebf48cf14aa7341864f4650
Kathryn Jerez <kathrynjerez@gmail.com>
2023-04-21 10:19:29 -0400

    firebase update

--- firebase-config.js
+++ firebase-config.js
@@ -7,13 +7,13 @@ import { getFirestore } from '@firebase/firestore';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId:  process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyA5p4ZJcG-23dCc_tGDhY2NWo9KEFCHXho",
+  authDomain: "lighthall-todo.firebaseapp.com",
+  projectId: "lighthall-todo",
+  storageBucket: "lighthall-todo.appspot.com",
+  messagingSenderId: "101275951677",
+  appId: "1:101275951677:web:9a82348363b519201b8054",
+  measurementId: "G-945L1GNL3Z"
 };
 
 // Initialize Firebase
